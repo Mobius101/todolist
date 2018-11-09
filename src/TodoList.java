f129bdb7c5b6031ab1054b806b5cbba73d883181
Aadam Zaman <Aadam.Zaman@mail.bcu.ac.uk>
2018-11-09 16:25:17 +0000

    pair programming task

--- src/TodoList.java
+++ src/TodoList.java
@@ -2,27 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TodoList {
+	List<TodoListItem> itemList = new ArrayList<TodoListItem>();
     /*
      * Add a new item to the list, given a description and priority.
      */
     public void addItem(String description, int priority) {
-        // TODO
+    	TodoListItem item = new TodoListItem(description, priority);
+    	itemList.add(item);
     }
     
     /*
      * Get the item at a given index.
      */
     public TodoListItem getItem(int i) {
-        // TODO
-        return null;
+        return itemList.get(i);
     }
     
     /*
      * Get the number of items in the list.
      */
     public int size() {
-        // TODO
-        return 0;
+        return itemList.size();
     }
     
     /*
@@ -30,7 +30,11 @@ public class TodoList {
      * If all items are marked "done", this method returns null.
      */
     public TodoListItem findFirstNotDone() {
-        // TODO
+        for(TodoListItem i : itemList) {
+        	if (i.isDone() == false) {
+        		return i;
+        	}
+        }
         return null;
     }
     
@@ -39,8 +43,15 @@ public class TodoList {
      * If all items are marked "done", this method returns null.
      */
     public TodoListItem findHighestPriorityNotDone() {
-        // TODO
-        return null;
+        TodoListItem hp = itemList.get(0);
+        for(TodoListItem i : itemList) {
+        	if (hp.getPriority() < i.getPriority()) {
+        		if (i.isDone() == false) {
+        			hp = i;
+        		}
+        	}
+        }
+        return hp;
     }
     
     /*
@@ -48,7 +59,7 @@ public class TodoList {
      * The descriptions are returned in a list.
      */
     public List<String> search(String text) {
-        // TODO
+        // accidentally destroyed because the undo button is right next to the save all button
         return null;
     }
 }
